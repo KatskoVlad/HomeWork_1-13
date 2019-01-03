8b0682a8293406b24380b660c4046f9bef0263a5
KatskoVlad <44637988+KatskoVlad@users.noreply.github.com>
2019-01-03 16:35:49 +0300

    Add files via upload

---
+++ homework/HomeWork12/TasksStreamMain.java
@@ -0,0 +1,35 @@
+package homework.HomeWork12;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TasksStreamMain {
+    public static void main(String[] args) {
+        // Task1
+        StreamTask1 streamTask1 = new StreamTask1();
+        try {
+            streamTask1.task1();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Task2
+        Path randomNumPath = Paths.get("src", "homework", "HomeWork12", "random.txt");
+        Path outputPathForSortNum = Paths.get("src", "homework", "HomeWork12", "sorted.txt");
+        int amountOfNumbers = 10;
+        int boundOfRandomizing = 100;
+
+        StreamTask2.fileWithRandomValues(amountOfNumbers, boundOfRandomizing, randomNumPath);
+        StreamTask2.getSortedNum(randomNumPath, outputPathForSortNum);
+
+        // Task3
+        StreamTask3 streamTask3 = new StreamTask3();
+        streamTask3.task3();
+
+
+    }
+}
